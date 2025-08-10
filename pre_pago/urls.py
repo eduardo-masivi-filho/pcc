18d7fa41dd34cba46439b97d5430bfc927e8e25b
eduardo masivi <eduardomasivi4@gmail.com>
2025-08-10 01:39:43 +0100

    adicionei páginas html:     cadastro, login, home, perfil, historico_pagamentos.
    Organizei de seguinte maneira:
    
        As páginas que servem para sessão estão na pasta sessao e as página de conteúdo (home, historico_pagamentos
        perfil) estão na pasta conteúdo.

--- pre_pago/urls.py
+++ pre_pago/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #       Início
     path('', views.index, name="index"),
     
+    #       Sessão
+    path('cadastro', views.cadastro, name="cadastro"),
+    path('login', views.login, name="login"),
+    
+    #       Conteúdo
+    path('home', views.home, name="home"),
+    path('historico', views.historico_de_pagamentos, name="historico"),
+    path('perfil', views.perfil, name="perfil"),
 ]
--- pre_pago/views.py
+++ pre_pago/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+#       Início
 def index(request):
     return render(request,"pre_pago/index.html")
+
+#       Sessão
+def cadastro(request):
+    return render(request, "pre_pago/sessao/cadastro.html")
+
+def login(request):
+    return render(request, "pre_pago/sessao/login.html")
+
+#       Conteúdo
+def home(request):
+    return render(request, "pre_pago/conteudo/home.html")
+
+def historico_de_pagamentos(request):
+    return render(request, "pre_pago/conteudo/historico_pagamentos.html")
+
+def perfil(request):
+    return render(request, "pre_pago/conteudo/perfil.html")
